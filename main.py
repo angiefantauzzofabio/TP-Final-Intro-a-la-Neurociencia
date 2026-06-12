d7a685ac7fd5eb650adcd652c068d9dbc008c955
angiefantauzzofabio <67520513+angiefantauzzofabio@users.noreply.github.com>
2026-06-12 15:32:08 -0300

    Cambios

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import librosa
 # Ruta de tu archivo MP3
 ruta_mp3 = './grupo a/A Night In Tunisia.mp3'
 
-# Cargar el archivo de audio
-y, sr = librosa.load(ruta_mp3, sr=22050)
+# Cargar el archivo de audio, lo hago durar un minuto y medio 
+y1, sr1 = librosa.load(ruta_mp3, sr=None, duration=5400.0) 
 
-print(f"Frecuencia de muestreo (sr): {sr} Hz")
-print(f"Forma de la matriz de audio (y): {y.shape}")
+print(f"Frecuencia de muestreo (sr): {sr1} Hz")
+print(f"Forma de la matriz de audio (y): {y1.shape}")
